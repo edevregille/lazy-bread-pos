f9cfca9e4eaa81bd29ebb54e1c713401b2fb5132
evregille <manu@metronome.com>
2025-01-23 14:55:45 -0800

    fix

--- src/config/authOptions.ts
+++ src/config/authOptions.ts
@@ -1,7 +1,7 @@
 import GoogleProvider from 'next-auth/providers/google';
 import { AuthOptions } from 'next-auth';
 
-const ALLOWED_EMAILS = ["manudevregille@gmail.com"]
+const ALLOWED_EMAILS = ["manudevregille@gmail.com", "lilliandevregille@gmail.com"]
 export const authOptions: AuthOptions = {
     providers: [
         GoogleProvider({
@@ -9,6 +9,7 @@ export const authOptions: AuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET  || "",
         })
     ],
+    secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
         async signIn({ user }) {
           if (user.email && ALLOWED_EMAILS.includes(user.email)) {
