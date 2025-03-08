a232588b692475add87d72c2d7b48d241724777d
evregille <manu@metronome.com>
2025-03-08 07:20:16 -0800

    update

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,23 +2,13 @@ import { Product } from "@/app/types";
 
 export const products: Product[] = [
   {
-    id: "soup",
-    name: "Soup 32oz",
-    unitCost: 13,
-  },
-  {
-    id: "broth",
-    name: "Broth 16oz",
-    unitCost: 5,
-  },
-  {
     id: "bread",
     name: "Foccacia Bread",
-    unitCost: 6,
+    unitCost: 6.5,
   },
   {
-    id: "meal_for_two",
-    name: "Meal For 2 (1 soup + 1 bread)",
-    unitCost: 18,
+    id: "sweat_rosmary",
+    name: "Sweat Rosmary",
+    unitCost: 4,
   }
 ];
